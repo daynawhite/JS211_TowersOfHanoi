51c8747af4794070e09045b8d91c69ac755e4ff7
daynawhite <dayna26.2@gmail.com>
2023-10-30 12:02:09 -0500

    DOM interaction for game play is working. Still need to add checkForWin function

--- index.js
+++ index.js
@@ -1,49 +1,92 @@
 
-
-let stone = null
-
-// this function is called when a row is clicked. 
-// Open your inspector tool to see what is being captured and can be used.
-
-
 const topC = document.getElementById('top-c')
 const middleB = document.getElementById('middle-b')
 const bottomA = document.getElementById('bottom-a')
 
+let stone = null
+let startRow = null
+
+const checkForWin = () => {
+  // if ((stacks.b.length == 4) || (stacks.c.length == 4)) {
+  // console.log('CONGRATS!  You won in only ' +counter + ' moves!');
+  // return true
+  } else {
+    // getPrompt();
+    return false
+}}
 
 const selectRow = (row) => {
-
   const currentRow = row.getAttribute("id")
-  
-  console.log("Yay, we clicked a row", currentRow)
+  // console.log("Yay, we clicked a row", currentRow)
   // console.log("Here is the stone's id: ", row.lastElementChild.id)
+  if (!stone) {
+    pickUpStone(row.id)
+} else {dropStone(row.id)
+}}
 
-  pickUpStone(currentRow)
-} 
-
-function pickUpStone(currentRow) {
-let element =  document.getElementById(currentRow)
+function pickUpStone(row) {
+let currentRow =  document.getElementById(row)
+startRow = currentRow.id
+console.log('startRow: ', startRow)
+// ADD CODE HERE: if currentRow contains no children, Alert('Try another row')
   // let stone = row.lastChild
   // console.log('the stones id', stone)
- element.removeChild(element.lastElementChild);
+ stone = currentRow.lastElementChild
+ currentRow.removeChild(stone);
 }
 
-document.getElementById('top-c').addEventListener(click, pickUpStone(topC))
-document.getElementById('middle-b').addEventListener(click, pickUpStone(middleB))
-document.getElementById('bottom-a').addEventListener(click, pickUpStone(bottomA))
+const isLegal = (endRow) => {
+  // ADD CODE to check for clicks outside the rows
+  let endRowElement = document.getElementById(endRow)
 
-// this function can be called to get the last stone in the stack
-// but there might be something wrong with it...
+  if (endRowElement.firstElementChild) {
+    console.log('endRow has blocks')
+    let endPiece = endRowElement.lastElementChild
+    let idOfStone = stone.getAttribute('id')
+    let idOfEndPiece = endPiece.getAttribute('id')
+    if (Number(idOfStone) < Number(idOfEndPiece)) {
+      console.log('LEGAL')
+      return true
+    } else {
+      console.log('illegal move. Try again!');
+      return false
+  }}
+  else {
+    console.log('endRow has no blocks')
+    return true
+  }
+} 
 
+const dropStone = (endRow) => {
+  if (isLegal(endRow)) {
+  let currentRow =  document.getElementById(endRow)
 
-// You could use this function to drop the stone but you'll need to toggle between pickUpStone & dropStone
-// Once you figure that out you'll need to figure out if its a legal move...
-// Something like: if(!stone){pickupStone} else{dropStone}
-
-const dropStone = (rowID, stone) => {
-  document.getElementById(rowID).appendChild(stone)
+  currentRow.appendChild(stone)
+  } else {
+    let previousRow = document.getElementById(startRow)
+    previousRow.appendChild(stone)
+    alert('Illegal Move! Try again.')
+  }
   stone = null
+  checkForWin()
 }
 
-// * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
+// document.getElementById('top-c').addEventListener(click, selectRow(row))
+// document.getElementById('middle-b').addEventListener(click, selectRow(row))
+// document.getElementById('bottom-a').addEventListener(click, selectRow(row))
 
+// document.getElementById('4').addEventListener(click, selectRow(yellow4))
+// document.getElementById('3').addEventListener(click, selectRow(red3))
+// document.getElementById('2').addEventListener(click, pickUpStone(green2))
+// document.getElementById('1').addEventListener(click, pickUpStone(blue1))
+
+let counter = 0
+const playGame = () => {
+    counter ++   
+
+  // if (isLegal(startStack, endStack) && isPossible()) {
+  //   isPossible(startStack, endStack)
+  //   movePiece(startStack, endStack)
+  //   checkForWin()
+  // }
+}
